1216bb1307fc06b90002203f7b6e85f2a939f490
Hedaya <hedaya.rafaat@gmail.com>
2017-12-09 23:10:36 +0200

    IR

---
+++ IR Avoiding Objects.ino
@@ -0,0 +1,39 @@
+int sensR= 8;
+int sensF= 7;
+int sensL= 9;
+void setup ()
+{
+pinMode(sensL, INPUT);
+  pinMode(sensR, INPUT);
+  pinMode(sensF, INPUT);
+}
+
+sL= digitalRead(sensL);
+ sF= digitalRead(sensF);
+ sR= digitalRead(sensR);
+void loop(){
+  sL= digitalRead(sensL);
+  sF= digitalRead(sensF);
+  sR= digitalRead(sensR);
+  
+  if (sF== HIGH) {
+    backward(5);
+  }
+  if (sF== LOW) {
+    if (sL==HIGH && sR== LOW){
+      right(2.5);
+    }
+    if (sL== LOW && sR== HIGH){
+      
+left(2.5);
+    }
+    if (sL== HIGH && sR == HIGH){
+     
+left(2.5);
+    }
+    if (sL== LOW && sR== LOW){
+      
+forward();
+    }  
+  }
+
